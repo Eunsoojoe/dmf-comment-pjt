4d4f3a2a1e7245f8bbbd1de91ef4b4efed524c96
Eunsoojoe <joeun2264@gmail.com>
2024-08-01 11:58:47 +0900

    Comment Read 기능 구현

--- articles/models.py
+++ articles/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    # comment_set =  
 
 class Comment(models.Model):
     content = models.TextField()
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)    # Article 내에 comment_set, Comment 내에 article_id가 자동생성
+    # article_id = 
 
 
