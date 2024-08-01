976722fce4427b9881731130a983d21e5cc4fe09
Eunsoojoe <joeun2264@gmail.com>
2024-08-01 11:40:04 +0900

    Comment 모델링 / 마이그레이션

--- articles/models.py
+++ articles/models.py
@@ -5,6 +5,8 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
 
-# class Comment(models.Model):
-#     pass
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
 
