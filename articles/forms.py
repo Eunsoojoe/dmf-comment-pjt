e87998ea7ece22545c5c54e0611fadef4a21dad2
Eunsoojoe <joeun2264@gmail.com>
2024-08-02 10:03:55 +0900

    form에 bootstrap 적용

--- articles/forms.py
+++ articles/forms.py
@@ -1,12 +1,25 @@
 from django import forms
 from .models import Article, Comment
 
+
+# 자동으로 생성된 폼 꾸며보기.
 class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+            }
+        )
+    )
+
+
+
     class Meta():
-        model = Article
+        model = Article    # 장고가 자동적으로 Article 구조에 맞게 form 생성
         fields = "__all__"
 
 class CommentForm(forms.ModelForm):
+    # CommentForm을 도와주는 부가적인 요소들.
     class Meta():
         model = Comment
         # fields = '__all__'
